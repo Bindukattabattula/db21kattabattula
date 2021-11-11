0f4f85ed03226fa3e8c4a613219308641bfb3dac
Bindukattabattula <89418288+Bindukattabattula@users.noreply.github.com>
2021-11-10 21:50:24 -0600

    added endpoints

--- routes/tiger.js
+++ routes/tiger.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('tiger', { title: 'Search Results by Tiger' });
-});
-
-module.exports = router;
+var express = require('express'); 
+const tiger_controlers= require('../controllers/tiger'); 
+var router = express.Router(); 
+ 
+/* GET tigers */ 
+router.get('/', tiger_controlers.tiger_view_all_Page ); 
+module.exports = router; 
