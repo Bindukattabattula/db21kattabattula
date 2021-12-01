e28ae9fad304e8b549dfe9ca7910af10a8d93b62
Bindukattabattula <89418288+Bindukattabattula@users.noreply.github.com>
2021-12-01 15:28:18 -0600

    Update tiger.js

--- routes/tiger.js
+++ routes/tiger.js
@@ -1,16 +1,25 @@
 var express = require('express');
 const tiger_controlers= require('../controllers/tiger');
 var router = express.Router();
+const secured = (req, res, next) => {
+  if (req.user){
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+}
 /* GET tiger */
 router.get('/', tiger_controlers.tiger_view_all_Page );
 /* GET detail tiger page */
 router.get('/detail', tiger_controlers.tiger_view_one_Page);
 /* GET create tiger page */
-router.get('/create', tiger_controlers.tiger_create_Page);
+router.get('/create', secured, tiger_controlers.tiger_create_Page);
 /* GET create update page */
-router.get('/update', tiger_controlers.tiger_update_Page);
+// router.get('/update', tiger_controlers.tiger_update_Page);
+/* GET update tiger page */
+router.get('/update', secured, tiger_controlers.tiger_update_Page);
 /* GET create tiger page */
-router.get('/delete', tiger_controlers.tiger_delete_Page);
+router.get('/delete', secured, tiger_controlers.tiger_delete_Page);
 
 
 module.exports = router;
